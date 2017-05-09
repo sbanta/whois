0617af3f1dd1ab9d77f690fffec4a31c46ac3973
Shawn Banta <shawn.banta@rallyhealth.com>
2017-05-09 15:21:35 -0700

    General updates

--- whois.py
+++ whois.py
@@ -6,7 +6,6 @@ import pandas
 
 dest_ip = pandas.read_csv("whois.csv", sep=',', usecols=['dest_ip'], squeeze=True)
 count = pandas.read_csv("whois.csv", sep=',', usecols=['count'], squeeze=True)
-outfile = outfile.csv
 key = 'description'
 output = []
 ips = []
@@ -20,4 +19,4 @@ for i in dest_ip:
         output.append('Lookup Error')
 
 df = pandas.DataFrame(list(zip(ips, count, output)))
-df.to_csv(outfile.csv, header=False)
+df.to_csv('outfile.csv', header=False)
