f5c10140ef0f94b3b0563d8ee9d4790044911313
Shawn Banta <shawn.banta@rallyhealth.com>
2017-05-09 12:42:05 -0700

    Update

--- whois.py
+++ whois.py
@@ -5,9 +5,13 @@ import csv
 with open('whois.csv', 'rb') as i:
     reader = csv.reader(i)
     ip_list = list(reader)
+    for i in ip_list:
+        obj = IPWhois(i)
+        res = obj.lookup_whois()
+        pprint(res)
 
-print(ip_list)
+#print(ip_list)
 
-obj = IPWhois('192.230.74.158')
-res=obj.lookup_whois()
-pprint(res)
+#0bj = IPWhois('192.230.74.158')
+#res=obj.lookup_whois()
+#pprint(res)
