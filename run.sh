b357d2b794088d0b9f4e14627350b807c5737837
Shawn Banta <shawn.banta@rallyhealth.com>
2017-05-09 15:19:31 -0700

    General updates

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 
 docker build -t whois-docker .
 docker run whois-docker python whois.py /tmp/whois.py
+docker cp /tmp/outfile.csv:~/Desktop/
--- whois.py
+++ whois.py
@@ -5,18 +5,19 @@ import csv, sys, argparse
 import pandas
 
 dest_ip = pandas.read_csv("whois.csv", sep=',', usecols=['dest_ip'], squeeze=True)
-print dest_ip
+count = pandas.read_csv("whois.csv", sep=',', usecols=['count'], squeeze=True)
 outfile = outfile.csv
 key = 'description'
+output = []
+ips = []
 for i in dest_ip:
-    print i
-    obj = IPWhois(i)
-    res = obj.lookup_whois()
-    #print(res['nets'][0][key])
-    res.to_csv(outfile, sep='\t')
+    try:
+        obj = IPWhois(i)
+        res = obj.lookup_whois()
+        ips.append(i)
+        output.append(res['nets'][0][key])
+    except:
+        output.append('Lookup Error')
 
-#print(ip_list)
-
-#0bj = IPWhois('192.230.74.158')
-#res=obj.lookup_whois()
-#pprint(res)
+df = pandas.DataFrame(list(zip(ips, count, output)))
+df.to_csv(outfile.csv, header=False)
