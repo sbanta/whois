2b76e98f629a5c038ff2c46e1c35a385c0e8cef3
Shawn Banta <shawn.banta@rallyhealth.com>
2017-05-10 13:12:06 -0700

    Testing build

--- run.sh
+++ run.sh
@@ -11,10 +11,13 @@ else
   docker build -t whois-docker .
   #docker cp $filepath whois-docker:/tmp/
   #docker run --name image_name whois-docker python whois.py /tmp/whois.py -i "/tmp/"$filepath -o outputtest.csv
-  docker cp $filepath whois-docker:/tmp/
-  docker run -it whois-docker python whois.py /tmp/whois.py -i "/tmp/"$filepath -o outputtest.csv
+  #docker cp $filepath whois-docker:/tmp/
+  #docker run -it whois-docker python whois.py /tmp/whois.py -i "/tmp/"$filepath -o outputtest.csv
+  docker run -it -d whois-docker /bin/bash
   alias dl='docker ps -l -q'
   docker start `dl`
+  docker cp $filepath `dl`:/tmp/
+  docker exec -it `dl` python whois.py /tmp/whois.py -i "/tmp/"$filepath -o outputtest.csv
   docker cp whois-docker:/tmp/outfiletest.csv ~/Desktop/
   docker stop `dl`
 fi
