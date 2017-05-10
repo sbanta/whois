a1dc7dc03140a0dad74825c224eeb1893be4ddd3
Shawn Banta <shawn.banta@rallyhealth.com>
2017-05-10 10:46:35 -0700

    Testing build

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ else
   export filepath
   docker build --no-cache=true -t whois-docker .
   docker cp $filepath 7a3db01a5762:/tmp/
-  docker run whois-docker python whois.py /tmp/whois.py -i $filepath -o outputtest.csv
+  docker run whois-docker python whois.py /tmp/whois.py -i "/tmp/"$filepath -o outputtest.csv
   docker cp 7a3db01a5762:/tmp/outfiletest.csv ~/Desktop/
 fi
