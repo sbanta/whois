fc24e3effc8e7f1fb8ef2bd093c7610167406b72
Shawn Banta <shawn.banta@rallyhealth.com>
2017-05-11 14:59:50 -0700

    Updated cp command

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ else
   docker cp $filepath testing:/app/
   docker exec -it testing ls /app/
   docker exec -it testing python whois.py -i "/app/"$filepath -o outputtest.csv
-  docker cp testing:/aoo/outfiletest.csv ~/Desktop/
+  docker cp testing:/app/outfiletest.csv .
   docker stop testing
   docker rm testing
 fi
