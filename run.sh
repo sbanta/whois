8de48f53bea09b27906e0622ec94e997f82d3816
Shawn Banta <shawn.banta@rallyhealth.com>
2017-05-11 14:57:22 -0700

    Added updated app path

--- run.sh
+++ run.sh
@@ -15,10 +15,10 @@ else
   #docker run -it whois-docker python whois.py /tmp/whois.py -i "/tmp/"$filepath -o outputtest.csv
   #docker run --name testing -it -d whois-docker /bin/bash
   docker start testing
-  docker cp $filepath testing:/tmp/
-  docker exec -it testing ls /tmp/
-  docker exec -it testing python whois.py -i "/tmp/"$filepath -o outputtest.csv
-  docker cp testing:/tmp/outfiletest.csv ~/Desktop/
+  docker cp $filepath testing:/app/
+  docker exec -it testing ls /app/
+  docker exec -it testing python whois.py -i "/app/"$filepath -o outputtest.csv
+  docker cp testing:/aoo/outfiletest.csv ~/Desktop/
   docker stop testing
   docker rm testing
 fi
