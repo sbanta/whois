457ff7641a548d3abc56380b65f9c20e2062e23d
Shawn Banta <shawn.banta@rallyhealth.com>
2017-05-11 16:47:58 -0700

    Fuck

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,11 @@ RUN mkdir /app
 # Set the working directory to /app
 WORKDIR /app
 
-RUN rm -rf /app
+#RUN rm -rf /app
 
 RUN git clone https://github.com/sbanta/whois.git /app
 
 # Install any needed packages specified in requirements.txt
 RUN pip install -r requirements.txt
 
-RUN git pull
+#RUN git pull
--- run.sh
+++ run.sh
@@ -9,11 +9,12 @@ else
   filepath=$1
   outfile=$2
   docker build -t whois-docker:1.0 .
-  docker run -d --name whois whois-docker:1.0
-  docker cp $filepath testing:/app/
-  docker exec -it whois ls /app/
+  docker run -d -t --name whois whois-docker:1.0
+  docker cp $filepath whois:/app/
+  #docker start whois
   docker exec -it whois python whois.py -i "/app/"$filepath -o $outfile
   docker cp whois:/app/$outfile .
   docker stop whois
-  docker rmi whoid-docker:1.0
+  docker rm whois
+  docker rmi whois-docker:1.0
 fi
