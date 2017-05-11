1b1157929385f7ee1ba974d60e575877de0c2535
Shawn Banta <shawn.banta@rallyhealth.com>
2017-05-11 16:05:46 -0700

    All the things

--- run.sh
+++ run.sh
@@ -7,18 +7,13 @@ if [ $# -eq 0 ]; then
   exit 1
 else
   filepath=$1
-  export filepath
-  docker build -t whois-docker .
-  #docker cp $filepath whois-docker:/tmp/
-  #docker run --name image_name whois-docker python whois.py /tmp/whois.py -i "/tmp/"$filepath -o outputtest.csv
-  #docker cp $filepath whois-docker:/tmp/
-  #docker run -it whois-docker python whois.py /tmp/whois.py -i "/tmp/"$filepath -o outputtest.csv
-  #docker run --name testing -it -d whois-docker /bin/bash
-  docker start testing
+  outfile=$2
+  docker build -t whois-docker:1.0 .
+  docker run -d --name whois whois-docker:1.0
   docker cp $filepath testing:/app/
-  docker exec -it testing ls /app/
-  docker exec -it testing python whois.py -i "/app/"$filepath -o outputtest.csv
-  docker cp testing:/app/outputtest.csv .
-  docker stop testing
-  docker rm testing
+  docker exec -it whois ls /app/
+  docker exec -it whois python whois.py -i "/app/"$filepath -o $outfile
+  docker cp whois:/app/$outfile .
+  docker stop whois
+  docker rmi whoid-docker:1.0
 fi
